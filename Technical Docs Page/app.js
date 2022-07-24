b23cb91955eac6133ebbe121499c23538353a7aa
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2022-07-24 16:34:28 +0800

    final

---
+++ Technical Docs Page/app.js
@@ -0,0 +1,28 @@
+// const navbar = document.querySelector("#navbar");
+// const mainContent = document.querySelector("#main-doc");
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+// //   if (navbar.style.height === "50px") {
+// //     navbar.style.height = "250px";
+// //   } else {
+// //     navbar.style.height = "50px";
+// //   }
+//   faTreaba()
+// });
+
+// var oldHeight = '';
+// function faTreaba () {
+//     const height = window.getComputedStyle(navbar).height
+//     const esteMare = parseInt(height) >= 275
+//     console.log(`esteMare: ${esteMare}, '${height}'`)
+//     if (esteMare) {
+//         oldHeight = height
+//         navbar.style.height = '50px'
+//     } else {
+//         navbar.style.height = oldHeight
+//     }
+// }
+// navbar.addEventListener("click", () => {
+//     faTreaba()
+//   })
