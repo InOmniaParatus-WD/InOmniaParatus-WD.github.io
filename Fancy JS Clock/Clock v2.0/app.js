3a2b8bbe67f63e5746be3147062d9f01d1c5dbbc
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2022-08-07 10:11:56 +0800

    v2.0 of the clock

---
+++ Fancy JS Clock/Clock v2.0/app.js
@@ -0,0 +1,79 @@
+const body = document.querySelector("body");
+document.body.onload = addElement;
+
+function addElement () {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("13");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const clockContainer = document.querySelector(".time-and-date");
+  document.body.insertBefore(newDiv, clockContainer);
+}
+
+const hourEl = document.querySelector(".hours");
+const minuteEl = document.querySelector(".minutes");
+const secondEl = document.querySelector(".seconds");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesaday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+
+//Changes the time and date displayed on the clock and the displays underneath it
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12; //12hrs instead of 24hrs time format
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hourEl.style.transform = ` translate(-50%, -100%) rotate(${scale(hoursForClock,0,12,0,360)}deg)`;
+  minuteEl.style.transform = ` translate(-50%, -100%) rotate(${scale(minutes,0,60,0,360)}deg)`;
+  secondEl.style.transform = ` translate(-50%, -100%) rotate(${scale(seconds,0,60,0,360)}deg)`;
+
+  timeEl.innerHTML = `${hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`; // For the non-military hour format with 24:00 hrs use 'hoursForClock''hours' instead of 
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span>${date}</span>`;
+}
+
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, inMin, inMax, outMin, outMax) => {
+  return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+setTime();
+setInterval(setTime, 1000);
