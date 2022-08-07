0b697e9937ca69fb9b8f8bc06fce5c1aa5905b6a
Ioana Popa <78725314+InOmniaParatus-WD@users.noreply.github.com>
2022-08-07 13:08:27 +0800

    Update app.js

--- Fancy JS Clock/app.js
+++ Fancy JS Clock/app.js
@@ -40,9 +40,9 @@ function setTime() {
   const seconds = time.getSeconds();
   const ampm = hours >= 12 ? "PM" : "AM";
 
-  hourEl.style.transform = ` translate(-50%, -100%) rotate(${scale(hoursForClock,0,12,0,360)}deg)`;
-  minuteEl.style.transform = ` translate(-50%, -100%) rotate(${scale(minutes,0,60,0,360)}deg)`;
-  secondEl.style.transform = ` translate(-50%, -100%) rotate(${scale(seconds,0,60,0,360)}deg)`;
+  hourEl.style.transform = ` translate(-50%, -100%) rotate(${scale(hoursForClock,0,11,0,359)}deg)`;
+  minuteEl.style.transform = ` translate(-50%, -100%) rotate(${scale(minutes,0,59,0,359)}deg)`;
+  secondEl.style.transform = ` translate(-50%, -100%) rotate(${scale(seconds,0,59,0,359)}deg)`;
 
   timeEl.innerHTML = `${hours}:${
     minutes < 10 ? `0${minutes}` : minutes
