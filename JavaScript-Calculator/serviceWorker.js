5c2552aad675fa702b5b0877257fa187a17491f0
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-05 15:40:27 +0800

    trying out PWA

---
+++ JavaScript-Calculator/serviceWorker.js
@@ -0,0 +1,27 @@
+// PWA
+const pocketCalculator = "Calculator";
+const assets = ["/", "/index.html", "/app.js", "/styles.css"];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(pocketCalculator).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
