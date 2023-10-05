32999cd863a5fc7ce06edee0dd2119b02f01c056
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-05 17:05:54 +0800

    install popup retry

--- JavaScript-Calculator/serviceWorker.js
+++ JavaScript-Calculator/serviceWorker.js
@@ -2,6 +2,33 @@
 const pocketCalculator = "Calculator";
 const assets = ["index.html", "app.js", "styles.css"];
 
+let promptUser;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  promptUser = e;
+  // Update the install UI to notify the user app can be installed
+  document.querySelector('#install_button').style.display = 'block';
+});
+
+document.querySelector('#install_button').addEventListener('click', (e) => {
+  // Hide the install button
+  document.querySelector('#install_button').style.display = 'none';
+  // Show the install prompt
+  promptUser.prompt();
+  // Wait for the user to respond to the prompt
+  promptUser.userChoice.then((res) => {
+    if (res.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
+
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
