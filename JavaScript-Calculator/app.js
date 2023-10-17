15da7431f1a89ead056bb062e04103b7bb4fa99f
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-17 16:12:43 +0800

    refactor JS code

--- JavaScript-Calculator/app.js
+++ JavaScript-Calculator/app.js
@@ -1,3 +1,5 @@
+import { Calculator } from "./classCalculator.js";
+
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const negativeNumbers = document.getElementById("negative");
@@ -7,187 +9,84 @@ const equals = document.getElementById("equals");
 const displayedNumber = document.getElementById("output-value");
 const displayedExpression = document.getElementById("history-value");
 
-let numberOrOperator = "";
-let expression = "";
-let evaluatedExpression = "";
+const pocketCalculator = new Calculator();
 
-// ------ FUNCTIONS TO MAKE THE CALCULATOR WORK ------
-
-//Updates the calculator display
 const updateDisplay = () => {
-  if (numberOrOperator !== "" && "+-/*".includes(numberOrOperator)) {
-    // pressed operator
-    displayedNumber.innerText = numberOrOperator;
-    displayedExpression.innerText = expression + numberOrOperator;
-  } else {
-    // number, deciml, negative
-    displayedNumber.innerText = numberOrOperator;
-    displayedExpression.innerText = expression;
-  }
-};
-//Adds a new digit to the number displayed every time the user hits a digit key before choosing an operator
-const appendNumber = (key) => {
-  //erases the result from the display when the user enters a new number following a completed previous calculation
-  if (evaluatedExpression) {
-    evaluatedExpression = "";
-    numberOrOperator = evaluatedExpression;
-    expression = "";
-  }
+  const [number, expression] = pocketCalculator.getCurrentState();
+  displayedNumber.innerText = number;
+  displayedExpression.innerText = expression;
+}
 
-  //prevents the user from adding more than one decimal dot
-  if (key === "." && numberOrOperator.includes(".")) return;
-
-  if ("+-/*".includes(numberOrOperator)) {
-    // verifies if the previously hit key is an operator
-    expression += numberOrOperator;
-    numberOrOperator = key;
-  } else {
-    numberOrOperator += key;
-  }
-
-  //A number cannot begin with zero unless zero is immediately followed by a decimal
-  if (
-    !isNaN(numberOrOperator) &&
-    numberOrOperator.startsWith("0") &&
-    numberOrOperator.length > 1 &&
-    numberOrOperator[1] !== "."
-  ) {
-    numberOrOperator = numberOrOperator.replace("0", "");
-  }
-};
-//The function allows the user to chose an operator and will replace the previously hit operator key. The operator dsiaplyed before the next integer is enatered will be the last operator key hit
-const chooseOperator = (operatorKey) => {
-  if (numberOrOperator === "") return;
-
-  if (evaluatedExpression) {
-    evaluatedExpression = "";
-    expression = numberOrOperator;
-  } else if (!"+-/*".includes(numberOrOperator)) {
-    if (!isNaN(numberOrOperator) && Number(numberOrOperator) < 0) {
-      numberOrOperator = `(${numberOrOperator})`;
-    }
-    expression += numberOrOperator;
-  }
-  numberOrOperator = operatorKey;
-};
-
-//Turns the displayed number into a negative
-const makeNumberNegative = () => {
-  if ("+-/*".includes(numberOrOperator)) return;
-  numberOrOperator = `${parseFloat(numberOrOperator) * -1}`;
-};
-
-//Delete the last entered number or digit
-const deleteKey = () => {
-  if (numberOrOperator === "") {
-    expression = expression.slice(0, -1);
-  }
-  numberOrOperator = numberOrOperator.slice(0, -1);
-};
-
-//Evaluates the math expression when the "equals" key is hit and returns the result
-//Uses Math.js library to avoid calculus errors with floating numbers
-
-const calculate = () => {
-  if (!isNaN(numberOrOperator)) {
-    if (Number(numberOrOperator) < 0) {
-      numberOrOperator = `(${numberOrOperator})`;
-    }
-  }
-  expression += numberOrOperator;
-  numberOrOperator = "";
-
-  let result = math.evaluate(expression);
-
-  evaluatedExpression = result.toLocaleString("en-US", {
-    maximumFractionDigits: 2,
-  });
-  numberOrOperator = evaluatedExpression;
-};
-
-// ---------------------- EVENT HANDLERS FOR EACH BUTTON --------------------
-
+// ------------ MOUSE EVENTS ------------- //
 numbers.forEach((button) =>
   button.addEventListener("click", (e) => {
-    appendNumber(e.target.dataset.number);
+    pocketCalculator.appendNumber(e.target.dataset.number);
     updateDisplay();
   })
 );
 
 operators.forEach((button) =>
   button.addEventListener("click", (e) => {
-    chooseOperator(e.target.dataset.operator);
+    pocketCalculator.chooseOperator(e.target.dataset.operator);
     updateDisplay();
   })
 );
 
 negativeNumbers.addEventListener("click", () => {
-  makeNumberNegative();
+  pocketCalculator.makeNumberNegative();
   updateDisplay();
 });
 
 equals.addEventListener("click", () => {
-  calculate();
+  pocketCalculator.calculate();
   updateDisplay();
 });
 
 clearDisplay.addEventListener("click", () => {
-  numberOrOperator = "";
-  expression = "";
+  pocketCalculator.clear();
   updateDisplay();
 });
 
 deleteLastKey.addEventListener("click", () => {
-  deleteKey();
+  pocketCalculator.delete();
   updateDisplay();
 });
-// ---------- DOCUMENT EVENT LISTENERS FOR KEYBOARD USE ------------
 
+
+// ------------ KEYBOARD EVENTS ------------- //
 document.addEventListener("keydown", (e) => {
   const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
   const operators = ["+", "-", "/", "*"];
   //Add digits to the numer
   if (digits.includes(e.key)) {
-    appendNumber(e.key);
+    pocketCalculator.appendNumber(e.key);
     updateDisplay();
-    console.log(e.key, "Number Key");
   }
   //Choose the operator
   if (operators.includes(e.key)) {
-    chooseOperator(e.key);
+    pocketCalculator.chooseOperator(e.key);
     updateDisplay();
-    console.log(e.key, "Operator Key");
   }
   //Calculate
   if (e.key === "=" || e.key === "Enter") {
-    calculate();
+    pocketCalculator.calculate();
     updateDisplay();
-    console.log(e.key, "Equal/Enter");
   }
   //Delete the last entered key
   if (e.key === "Backspace") {
-    deleteKey();
+    pocketCalculator.delete();
     updateDisplay();
-    console.log("delete last key");
   }
   //Clear display
   if (e.key === "Delete") {
-    numberOrOperator = "";
-    expression = "";
+    pocketCalculator.clear();
     updateDisplay();
-    console.log("clear the display");
   }
 });
 //Negate number
 document.addEventListener("keypress", (e) => {
-  if (!isNaN(numberOrOperator) && e.key === "_") {
-    makeNumberNegative();
+  if (e.key === "_") {
+    pocketCalculator.makeNumberNegative();
     updateDisplay();
   }
-  console.log(e.key);
 });
-
-// //MathJS example of evaluating a string expression
-let str = "2*3-4/5+22*54";
-console.log(str + " = " + math.evaluate(str));
-
---
+++ JavaScript-Calculator/classCalculator.js
@@ -0,0 +1,96 @@
+export class Calculator {
+  constructor() {
+    this.numberOrOperator = "";
+    this.expression = "";
+    this.evaluatedExpression = "";
+  }
+
+  getCurrentState() {
+    if (
+      this.numberOrOperator !== "" &&
+      "+-/*".includes(this.numberOrOperator)
+    ) {
+      return [this.numberOrOperator,  this.expression + this.numberOrOperator];
+    } else {
+        
+      return [this.numberOrOperator,  this.expression ];
+    }
+  }
+
+  appendNumber(key) {
+    if (this.evaluatedExpression) {
+      this.evaluatedExpression = "";
+      this.numberOrOperator = this.evaluatedExpression;
+      this.expression = "";
+    }
+
+    if (key === "." && this.numberOrOperator.includes(".")) return;
+
+    if ("+-/*".includes(this.numberOrOperator)) {
+      this.expression += this.numberOrOperator;
+      this.numberOrOperator = key;
+    } else {
+      this.numberOrOperator += key;
+    }
+
+    if (
+      !isNaN(this.numberOrOperator) &&
+      this.numberOrOperator.startsWith("0") &&
+      this.numberOrOperator.length > 1 &&
+      this.numberOrOperator[1] !== "."
+    ) {
+      this.numberOrOperator = this.numberOrOperator.replace("0", "");
+    }
+    console.log(key);
+  }
+
+  chooseOperator(key) {
+    if (this.numberOrOperator === "") return;
+
+    if (this.evaluatedExpression) {
+      this.evaluatedExpression = "";
+      this.expression = this.numberOrOperator;
+    } else if (!"+-/*".includes(this.numberOrOperator)) {
+      if (!isNaN(this.numberOrOperator) && Number(this.numberOrOperator) < 0) {
+        this.numberOrOperator = `(${this.numberOrOperator})`;
+      }
+      this.expression += this.numberOrOperator;
+    }
+    this.numberOrOperator = key;
+  }
+
+  makeNumberNegative() {
+    if ("+-/*".includes(this.numberOrOperator)) return;
+    this.numberOrOperator = `${parseFloat(this.numberOrOperator) * -1}`;
+  }
+
+  delete() {
+    if (this.numberOrOperator === "") {
+      this.expression = this.expression.slice(0, -1);
+    }
+    this.numberOrOperator = this.numberOrOperator.slice(0, -1);
+  }
+
+  clear() {
+    this.numberOrOperator = "";
+    this.evaluatedExpression = "";
+    this.expression = "";
+  }
+
+  calculate() {
+    if (!isNaN(this.numberOrOperator)) {
+      if (Number(this.numberOrOperator) < 0) {
+        this.numberOrOperator = `(${this.numberOrOperator})`;
+      }
+    }
+    this.expression += this.numberOrOperator;
+    this.numberOrOperator = "";
+
+    let result = math.evaluate(this.expression);
+
+    this.evaluatedExpression = result.toLocaleString("en-US", {
+      maximumFractionDigits: 3,
+    });
+    this.numberOrOperator = this.evaluatedExpression;
+  }
+}
