53fdb41fe4eeb80d47b11f9c99fbff7a333f41c1
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-18 16:37:09 +0800

    PWA fixed

--- Expense-Tracker/serviceWorker.js
+++ Expense-Tracker/serviceWorker.js
@@ -4,38 +4,9 @@ const assets = [
   "index.html",
   "script.js",
   "css/styles.css",
-  "css/modals-styles.css",
-  "css/scroll-styles.css",
-  "css/tooltip-styles.css",
   "images/expense-tracker.png"
 ];
 
-// let promptUser;
-
-// window.addEventListener("beforeinstallprompt", (e) => {
-//   promptUser = e; 
-//   document.querySelector("#install").style.display = "block";
-// });
-
-// document.querySelector("#install").addEventListener("click", (e) => {
-//   promptUser.prompt();
-//   promptUser.userChoice.then((res) => {
-//     if (res.outcome === "accepted") {
-//       console.log("User accepted the install prompt");
-//     } else {
-//       console.log("User dismissed the install prompt");
-//     }
-//   });
-// });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
-}
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
@@ -52,3 +23,12 @@ self.addEventListener("fetch", (evt) => {
     })
   );
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
