6157ee7e29ad9e4137f3fac990927900488f7f1d
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-20 10:34:55 +0800

    📝 update assests list

--- Music-Player/serviceWorker.js
+++ Music-Player/serviceWorker.js
@@ -1,15 +1,27 @@
 const player = "Music Player";
 
 const assets = [
+  // app files
   "index.html",
   "script.js",
   "styles.css",
+  // images
   "images/headphones.jpg",
   "images/instruments.jpg",
   "images/key.jpg",
-  "images/rock'n'roll.jpg"
+  "images/rock'n'roll.jpg",
+  // tracks
+  "music/Ambient Classical Guitar - William King.mp3",
+  "music/Inspiring Cinematic Ambient - Lexin Music.mp3",
+  "music/Into the Night - Prazkhanal.mp3",
+  "music/Leva Eternity - lemonmusicstudio.mp3",
+  "music/Lifelike - AlexiAction.mp3",
+  "music/Password Infinity - Evgeny Bardyuzha.mp3",
+  "music/Powerful Beat - penguinmusic.mp3",
+  "music/Slow Trap - Anton Vlaslov.mp3",
+  "music/Summer Walk - Olexy.mp3",
+  "music/Weeknds - DayFox.mp3",
 ];
-
 
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
