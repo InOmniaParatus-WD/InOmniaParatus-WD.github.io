ea3d76986c324814adfa97cc8ed4772c3adb48c3
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-20 10:49:51 +0800

    📱→💻 PWA

---
+++ Exchange-Rate-Calculator/sw.js
@@ -0,0 +1,34 @@
+// PWA
+const calculator = "Exchange Calculator";
+const assets = [
+  "index.html",
+  "script.js",
+  "styles.css",
+  "images/down-arrow-download-svgrepo-com.png",
+  "images/exchange-reates.png",
+];
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(calculator).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
+  );
+});
