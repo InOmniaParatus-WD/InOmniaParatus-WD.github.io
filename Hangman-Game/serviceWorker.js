6d8cc43731296185cab435440ee07fa6abbf4f73
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-25 12:13:56 +0800

    💻→📱PWA files added

---
+++ Hangman-Game/serviceWorker.js
@@ -0,0 +1,34 @@
+const expenseTracker = "Expense Tracker";
+
+const assets = [
+  "index.html",
+  "script.js",
+  "css/styles.css",
+  "images/expense-tracker.png"
+];
+
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(expenseTracker).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
+  );
+});
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
