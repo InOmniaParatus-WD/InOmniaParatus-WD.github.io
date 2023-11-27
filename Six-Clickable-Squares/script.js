8eab89ec475885107c43c67bb7edf79c335e4a47
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-11-27 11:18:25 +0800

    new challange solved

---
+++ Six-Clickable-Squares/script.js
@@ -0,0 +1,59 @@
+// STEP 3: Bring in all the square and the image
+// The image is not a feature required by the challange :)
+const squares = document.querySelectorAll(".square");
+const image = document.querySelector("img");
+
+// STEP 4: Keep track of the clicked squares
+// Initialize an array which will "house" all the clicked squares
+let clickedSquares = [];
+
+let startImg = `<img src="owl.png">`;
+
+squares.forEach((btn) => {
+  btn.innerHTML = startImg;
+  btn.addEventListener("click", (e) => {
+    // Save the value of the clicked square in a variable. Not mandatory but it prevents repeating myself
+    let clicked = e.target;
+
+    // Add the class green when clicked
+    clicked.classList.add("green");
+
+    // Disable squares once they are clicked to avoid multiple clicks
+    // If the quares are clicked once the colour reverse function gets triggered it will cause issues
+    clicked.style.pointerEvents = "none";
+
+    // Check if the button is already in the array if not push index
+    for (let i = 0; i < squares.length; i++) {
+      // Temporarly replace the image with a number. I will restore it later on.
+      clicked.innerHTML = clickedSquares.indexOf(clicked) + 1;
+      
+      // If the button is not there...
+      if (!clickedSquares.includes(clicked)) {
+        //...add it to the array
+        clickedSquares.push(clicked);
+      } 
+    }
+
+    // When the last button is clicked start changing colors backwards
+    if (clickedSquares.lastIndexOf(clicked) === squares.length - 1) {
+      changeColor();
+    }
+  });
+});
+
+// When the sixth button is clicked change colors back to white in reverse order
+function changeColor() {
+  for (let i = 5; i >= 0; i--) {
+    // Change the square's color and background in reverse order
+    setTimeout(() => {
+      // Bring the image back
+      clickedSquares[i].innerHTML = startImg;
+      // Remove the "green" class
+      clickedSquares[i].classList.remove("green");
+      // Empty the array of clicked squares so that the game can be played again
+      clickedSquares.pop();
+    }, (6 - i) * 1000);
+    // Re-enable the squares to be clickable again
+    setTimeout(() => (squares[i].style.pointerEvents = "initial"), 6000);
+  }
+}
