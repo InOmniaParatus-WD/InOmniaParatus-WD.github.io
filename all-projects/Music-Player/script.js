6bdff15f05d3993c9c50cd4e4e977d3427a00e8d
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2024-01-10 13:04:05 +0800

    updated photo collection

--- all-projects/Music-Player/script.js
+++ all-projects/Music-Player/script.js
@@ -36,7 +36,7 @@ const allSongs = [
 
 const songsCopy = JSON.parse(JSON.stringify(allSongs));
 
-const allImages = ["listening", "vinil", "headphones"];
+const allImages = ["listening", "vinil-colour", "headphones", "clef-dark", "listening-looking-ahead"];
 
 // Initially load song details into DOM
 showSongsInPlaylist();
